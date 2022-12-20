89f40a9ae835ec564a691bea9e2ac72a9cbf4199
sansheng <sansheng9@yandex.ru>
2022-12-20 15:11:06 +1000

    add comments and refactor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 bot = telebot.TeleBot('5891154215:AAEc37wi9CZjsq5NGnC73MHVC7M9vugvdjs')
 
 
-# Start weather bot
+# The starting message of the bot || Стартовое сообщение бота
 @bot.message_handler(commands=['start'])
 def start(message):
     smile_message = "⛅️"
@@ -18,52 +18,63 @@ def start(message):
     q_message = "Погоду из какого города мне показать?"
     bot.send_message(message.chat.id, q_message, parse_mode='html')
 
-
+#  Display information about the weather || Выводим информацию о погоде
 @bot.message_handler()
 def get_weather(message):
     if message.content_type == 'text':
+        # Send a user request to get an array of data and an image || Отправляем запрос пользователя, чтобы получить массив данных и картинку
         data = get_weather_api(message.text)
         if data is not None:
+            # Separating the array elements for ease of understanding || Отделяем элементы массива для простоты понимания
             weather = data[0]
             icon = data[1]
+            # Creating a button in which we pass two parameters || Создаем кнопку, в которой мы передаем два параметра
             markup = types.InlineKeyboardMarkup()
             button_sub = types.InlineKeyboardButton(text='Подписаться', callback_data=f'sub {message.text}')
             markup.add(button_sub)
+            # And create message || И создаем сообщение
             bot.send_photo(message.chat.id, icon)
             bot.send_message(message.chat.id, weather, parse_mode='html', reply_markup=markup)
             bot.send_message(message.chat.id, "Погоду из какого города мне показать?", parse_mode='html')
+        # If get an error in the request, the function returns None || Если получаем ошибку в запросе, то функция возвращает None
         if data is None:
             bot.send_message(message.chat.id, 'Попробуй другой город', parse_mode='html')
-        # print('Data: ' + str(data) + '\nMessage: ' + str(message.text))
 
 
+# Subscription function is called every time at a user-defined time interval || Функция подписки, вызывается каждый раз в определенный пользователем интервал времени
 def sub_weather(city, num, text):
+    # Send a user request to get an array of data and an image || Отправляем запрос пользователя, чтобы получить массив данных и картинку
     data = get_weather_api(city)
+    # I decided that the picture is unwanted here || Принял решение, что картинка здесь лишняя
     weather = data[0]
+    # Creating a button where the user selects a time interval || Создаем кнопку, где пользователь выбирает интервал времени
     markup = types.InlineKeyboardMarkup()
     unsub_button = types.InlineKeyboardButton(text='Отписаться', callback_data='unsub unsub')
     change_time_button = types.InlineKeyboardButton(text='Частота получения погоды', callback_data='time unsub')
     markup.add(unsub_button, change_time_button)
     bot.send_message(num, weather, parse_mode='html', reply_markup=markup)
 
-
+# A function that catches button clicks || Функция которая отлавливает нажатия на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    global sub
+    # We divide the incoming string into an array of two elements, the first of which defines the function of the button, and the second the parameters that are necessary to perform this function, example req = ['sub', 'Paris']
+    # || Приходящую строку делим на массив из двух элементов, первый из которых определяет функцию кнопки, а второй параметры, которые необходимы для выполнения этой функции, например req = ['sub', 'Владивосток']
     req = call.data.split()
-    seconds = 2 * 60 * 60
     global town
-    # args = [req[1], call.message.chat.id, call.message.text]
-    # timer = threading.Timer(time, sub_weather, args=args)
+    global sub
+    seconds = 2 * 60 * 60
+    # Subscription button || Кнопка подписки
     if req[0] == 'sub':
         bot.send_message(call.message.chat.id, 'Каждые два часа я буду присылать вам погоду 😳', parse_mode='html')
         town = req[1]
         sub = True
         print(req)
+    # Unsubscribe button || Кнопка отписки
     if req[0] == 'unsub':
         sub = False
         bot.send_message(call.message.chat.id, "Погоду из какого города мне показать?", parse_mode='html')
         print(req)
+    # Interval change button || Кнопка смены интервала
     if req[0] == 'time':
         sub = False
         markup = types.InlineKeyboardMarkup()
@@ -74,11 +85,13 @@ def callback_query(call):
         markup.add(item1, item2, item3, item4)
         bot.send_message(call.message.chat.id, "Через сколько мне показывать погоду?", parse_mode='html', reply_markup=markup)
         print(req)
+    # Interval time selection button || Кнопка выбора времени интервала
     if req[0] == 'change':
         seconds = int(req[1]) * 60 * 60
         sub = True
         bot.send_message(call.message.chat.id, f"Я буду показывать погоду каждый {req[1]} часа", parse_mode='html')
         print(req)
+    # Timer || Таймер
     while sub:
         time.sleep(seconds)
         sub_weather(town, call.message.chat.id, call.message.text)
--- weather_api.py
+++ weather_api.py
@@ -1,5 +1,4 @@
 import requests
-from wind_direction import get_wind_direction
 
 
 def get_weather_api(city):
@@ -7,7 +6,7 @@ def get_weather_api(city):
     lang = 'ru'
     try:
         response = requests.get(
-                f'https://api.openweathermap.org/data/2.5/weather?q={str(city)}&appid={API_key}&lang={lang}&units=metric')
+            f'https://api.openweathermap.org/data/2.5/weather?q={str(city)}&appid={API_key}&lang={lang}&units=metric')
         data = response.json()
         city_name = data["name"]
         weather_desc = data["weather"][0]["description"]
@@ -18,7 +17,9 @@ def get_weather_api(city):
         wind_speed = data['wind']['speed']
         wind_deg = data['wind']['deg']
         wind_direction = get_wind_direction(wind_deg)
-        weather = [f'В городе {city_name} сейчас {weather_desc}!\nТемпература: <b>{temp} °C</b>, чувствуется как <b>{feel_like} °C</b>.\nВетер <b>{wind_direction}</b>, <b>{wind_speed}</b> метров в секунду.', link] #<img src="https://openweathermap.org/img/wn/{icon}@2x.png"></img>
+        weather = [
+            f'В городе {city_name} сейчас {weather_desc}!\nТемпература: <b>{temp} °C</b>, чувствуется как <b>{feel_like} °C</b>.\nВетер <b>{wind_direction}</b>, <b>{wind_speed}</b> метров в секунду.',
+            link]
 
         return weather
     except Exception as ex:
@@ -26,3 +27,21 @@ def get_weather_api(city):
         weather = None
         return weather
 
+
+def get_wind_direction(deg):
+    if deg >= 326.25 or deg <= 33.75:
+        return 'северный'
+    elif deg >= 33.76 and deg <= 56.25:
+        return 'северо-восточный'
+    elif deg >= 56.26 and deg <= 123.75:
+        return 'восточный'
+    elif deg >= 123.75 and deg <= 168.75:
+        return 'юго-восточный'
+    elif deg >= 168.76 and deg <= 213.75:
+        return 'южный'
+    elif deg >= 213.76 and deg <= 258.75:
+        return 'западно-восточный'
+    elif deg >= 258.76 and deg <= 303.75:
+        return 'западный'
+    elif deg >= 303.76 and deg <= 326.24:
+        return 'северо-западный'
